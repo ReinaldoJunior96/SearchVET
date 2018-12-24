27bbea59185529f426185406efe51aaf09ad56ef
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2018-12-24 16:08:29 -0300

    opa opa

--- Pagamento.php
+++ Pagamento.php
@@ -22,7 +22,7 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
       <span class="navbar-toggler-icon"></span>
     </button>
-    aaaaaaaaaaaaaaaaaaaaaaaaaa
+    
     <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item">
