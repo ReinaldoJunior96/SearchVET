e8b52b47d12213c58f7fcb3793a014123afa930c
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2019-03-04 01:38:02 -0300

    Tirando UEMA da conexão

--- ClassesDAO/conexao.php
+++ ClassesDAO/conexao.php
@@ -9,7 +9,7 @@ class PDOconectar{
 	public function conectar(){
 		try{
 		//$pdo=new PDO("mysql:host=br912.hostgator.com.br;dbname=vetmap88_vetmaps","vetmap88_vetmaps","reinaldo@123");
-		$pdo=new PDO("mysql:host=localhost;dbname=vetmaps","uema","369258147");
+		$pdo=new PDO("mysql:host=localhost;dbname=vetmaps","root","");
 			return $pdo;
 		}catch( PDOException $ex ){ echo "Erro: ".$ex->getMessage(); }
 	}
